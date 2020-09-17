4026c2c20a9cc2fbfc74678bf061a8f79a60905f
bhekisisanyanda-expleo <bhekisisa.nyanda@exploegroup.com>
2020-09-17 16:57:09 +0200

    QA2QEPROJECT API

--- src/test/java/stepdefinitions/searchstepdefinitions.java
+++ src/test/java/stepdefinitions/searchstepdefinitions.java
@@ -56,7 +56,7 @@ public class searchstepdefinitions
     }
 
     @Given("enters a product name in the search field")
-    public void enters_a_product_name_in_the_search_filed()
+    public void enters_a_product_name_in_the_search_field()
     {
         // Write code here that turns the phrase above into concrete actions
         headers.put("Content-Type", "application/json");
